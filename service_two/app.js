8d1dc1306ee3dbc90016415cdf6c0a29844e2b46
ismailkhandokar0 <ismailkhandokar0@gmail.com>
2024-03-02 15:38:30 +0600

    update

--- service_two/app.js
+++ service_two/app.js
@@ -1,19 +1,44 @@
-const express = require('express');
-const User = require('./models/userModal');
-const mongoose = require('mongoose');
+import express from "express";
+import configureOpenTelemetry from "./tracing.js"; // Assuming tracing.js is the correct filename and extension
+import User from './models/userModal.js';
+import mongoose from 'mongoose';
+import { trace, context, propagation } from "@opentelemetry/api";
 
 const app = express();
 const PORT = 4007
 
+// Configure OpenTelemetry
+configureOpenTelemetry("validate");
+
 // MongoDB setup
-mongoose.connect('mongodb://root:example_password@mongodb:27017/test', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((error) => {
-    console.error("Failed to connect to MongoDB", error);
-});
+// mongoose.connect('mongodb://root:example_password@mongodb:27017/test', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log("Connected to MongoDB");
+// }).catch((error) => {
+//     console.error("Failed to connect to MongoDB", error);
+// });
+
+app.get("/validateuser", (req, res) => {
+    const ctx = propagation.extract(context.active(), req.headers); // Extract context from incoming request headers
+    const tracer = trace.getTracer("express-tracer");
+    console.log("Incoming request headers:", req.headers);
+    console.log(
+      "Extracted span from context:",
+      trace.getSpan(ctx)?.spanContext()
+    ); // Retrieve span from extracted context
+  
+    const span = tracer.startSpan(
+      "validate-user",
+      {
+        attributes: { "http.method": "GET", "http.url": req.url },
+      },
+      ctx
+    );
+    span.end();
+    res.json({ success: true });
+  });
 
 // API endpoint to check MongoDB
 app.get('/check-mongo', (req, res) => {
