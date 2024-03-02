88f7b303bf6d0359ac7dae5b0c64ff0d5c18a725
alamincse97 <alamin.cse97@gmail.com>
2024-03-02 14:26:04 +0600

    added to server 2

--- backend_server_2/app.js
+++ backend_server_2/app.js
@@ -1,10 +1,30 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send("Welcome to Backend Server 2");
+// });
+
+// app.listen(4001, ()=>{
+//     console.log("listening to post 4001");
+// });
+
 const express = require('express');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Welcome to Backend Server 2");
+
+app.post('/backend_server_2', (req, res) => {
+    
+    console.log('Received request from Backend Server 1');
+
+    res.json(
+        {
+            message: "Hello from server 2"
+        }
+    );
 });
 
-app.listen(4001, ()=>{
-    console.log("listening to post 4001");
+app.listen(4001, () => {
+    console.log('Backend Server 2 listening on port 4001');
 });
