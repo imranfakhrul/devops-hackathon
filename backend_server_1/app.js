9d42ce10e165e5b65c477d58ad13db17c4beda5b
alamincse97 <alamin.cse97@gmail.com>
2024-03-02 14:25:05 +0600

    added to server_1

--- backend_server_1/app.js
+++ backend_server_1/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Welcome to Backend Server 1");
+app.get('/trigger', async(req, res) => {
+    try{
+        const r = await axios.post('http://localhost:4001/backend_server_2');
+        // res.send('Request to Backend Server 2 Successful');
+        res.json(r.data)
+    } catch (error){
+        console.error('Error making request to Backend Server 2:', error);
+        res.status(500).send('Error making Backend Server 2');
+    }
 });
 
 app.listen(4000, ()=>{
